0fcc582366d51c3aace6d1eba5ac7540a2c2d12a
purvamarwade <purvamarwade815@gamil.com>
2025-05-02 23:01:15 +0530

    https://github.com/purvamarwade/Marchcarzone-project

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+
+    def __str__(self):
+
+        return self.first_name
+
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=200)
+    designation=models.CharField(max_length=255)
+    photo=models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link=models.URLField(max_length=100)
+    twitter_link=models.URLField(max_length=100)
+    google_plus_link=models.URLField(max_length=100)
+    created_date=models.DateTimeField(auto_now=True)
